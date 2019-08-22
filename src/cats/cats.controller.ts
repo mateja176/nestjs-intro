fa0e6620fb08adc0a0bc5133aad4d59c67dd2551
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-23 00:03:08 +0200

    feat: adds cat data transfer object

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,15 @@
 import {
+  Body,
   Controller,
   Get,
   Header,
-  HttpCode,
   Param,
   Post,
   Req,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable, of } from 'rxjs';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
@@ -23,8 +24,7 @@ export class CatsController {
   }
   @Post()
   @Header('Cache-Control', 'none')
-  @HttpCode(204)
-  create() {
+  create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
   @Get('ab*cd')
