c1f3670fdc2df91ed8021b69fcb549484705bc93
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-22 23:54:41 +0200

    feat: @Post() create

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
@@ -6,5 +6,11 @@ export class CatsController {
   @Get()
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  }
+  @Post()
+  @Header('Cache-Control', 'none')
+  @HttpCode(204)
+  create() {
+    return 'This action adds a new cat';
   }
 }
