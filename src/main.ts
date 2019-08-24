9184c7109e3aa5ade6f4162e0b89f66feece6643
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-24 20:25:10 +0200

    feat: uses ValidationPipe from @nestjs/common
    set transform to true if you'd like for your dtos to be instantiated
    this is useful in case the dto class contains methods

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassValidationPipe } from './cats/class-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ClassValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: false }));
 
   await app.listen(3000);
 }
