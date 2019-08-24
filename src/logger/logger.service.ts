f1bad37b70007cd7ee8d4cfac766bb93257d7d64
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-24 02:41:11 +0200

    feat: middleware may be defined as a function instead
    if it doesn't have dependencies

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Handler } from 'express';
 
+export const loggerMiddleware: Handler = (req, res, next) => {
+  console.log(req.method);
+
+  next();
+};
+
 @Injectable()
 export class LoggerService implements NestMiddleware {
-  use(...params: Parameters<Handler>) {
-    const [req, res, next] = params;
-
-    console.log('Request...');
-
-    next();
-  }
+  use = loggerMiddleware;
 }
