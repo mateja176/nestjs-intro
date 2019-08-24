85d01681b8cdc930b2feb11a8a81d5fea39d20bc
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-25 01:59:53 +0200

    feat: npm i -D @nestjs/testing

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Cats } from './cat.interface';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 
-describe('Cats Controller', () => {
-  let controller: CatsController;
+describe('CatsController', () => {
+  let catsController: CatsController;
+  let catsService: CatsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CatsController],
-    }).compile();
-
-    controller = module.get<CatsController>(CatsController);
+  beforeEach(() => {
+    catsService = new CatsService();
+    catsController = new CatsController(catsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result: Cats = [
+        {
+          name: 'alf',
+          age: 3,
+          breed: 'martian',
+        },
+      ];
+
+      jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
+
+      expect(await catsController.findAll()).toBe(result);
+    });
   });
 });
