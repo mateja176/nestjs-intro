038f60ae5aeac9f19869d16f56d67dd1c1f22c03
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-25 00:22:54 +0200

    feat(roles): adds hasRole check in guard

--- src/roles.guard.ts
+++ src/roles.guard.ts
@@ -1,12 +1,29 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { metaDataKey, User } from './models';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>(
+      metaDataKey.roles,
+      context.getHandler(),
+    );
+
+    if (!roles) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
-    return true;
+
+    const user: User = request.user;
+
+    const hasRole = () => user.roles.some(role => roles.includes(role));
+
+    return user && user.roles && hasRole();
   }
 }
