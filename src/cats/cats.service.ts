693a1fbe919df328cbbfcda510252f71649e4e45
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-25 20:37:47 +0200

    feat(cats.service): returns mock data

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as faker from 'faker';
+import { range } from 'lodash';
 import { Cat, Cats } from './cat.interface';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Cats = [];
+  private readonly cats: Cats = range(
+    0,
+    faker.random.number({ min: 1, max: 10 }),
+  ).map(() => ({
+    name: faker.name.firstName(),
+    age: faker.random.number({ min: 1, max: 10 }),
+    breed: faker.lorem.word(),
+  }));
 
   create(cat: Cat) {
     this.cats.push(cat);
