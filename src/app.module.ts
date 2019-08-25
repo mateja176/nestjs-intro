7f055b5446f7ba64d196d76343915f324d2c337a
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-25 15:07:41 +0200

    feat: adds global middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(cors(), compression(), LoggerService)
-      .forRoutes({ path: 'cats', method: RequestMethod.ALL });
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
