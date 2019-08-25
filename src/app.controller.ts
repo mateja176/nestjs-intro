0424f898288f38a10fb46866f6a3518f2da022a5
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-25 18:31:18 +0200

    feat(app): adds login handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request as IRequest } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req: IRequest) {
+    return req.user;
   }
 }
