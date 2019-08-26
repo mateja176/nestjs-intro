b330c304682eb225551f5275813eb5e0d18cd5b5
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-26 22:19:42 +0200

    test(app.controller): fix

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,19 +1,36 @@
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { jwtSecret } from './auth/constants';
+import { passportStrategy } from './models';
+import { UsersService } from './users/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: jwtSecret,
+        }),
+        PassportModule.register({
+          defaultStrategy: passportStrategy.jwt,
+        }),
+      ],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, AuthService, UsersService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
+  // it('should return "Hello World!"', () => {
+  //   expect(appController.getHello()).toBe('Hello World!');
+  // });
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
