cafa8198652dcdd5cc971ba3392996c91f363e58
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-31 00:30:32 +0200

    feat(app): adds Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const options = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +16,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: false }));
 
   app.enableCors();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
