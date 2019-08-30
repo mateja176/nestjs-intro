6e55c10837da88c97210d5c2370d4653336cb801
Mateja Petrovic <ma.petrovic@levi9.com>
2019-08-30 23:03:49 +0200

    feat(env): validates environment variables using Joi.validate

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,11 +1,51 @@
+import * as Joi from '@hapi/joi';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+const Envs = ['development', 'production', 'test', 'provision'] as const;
+type NodeEnv = typeof Envs[number];
+const Env = Envs.reduce((nodeEnv, env) => ({ ...nodeEnv, [env]: env }), {} as {
+  [key in NodeEnv]: key
+});
+
+export interface ValidatedEnvConfig {
+  NODE_ENV: NodeEnv;
+  PORT: number;
+  API_AUTH_ENABLED: boolean;
+}
+
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  private envConfig: ValidatedEnvConfig;
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+    const config = dotenv.parse(fs.readFileSync(filePath));
+
+    this.envConfig = this.validateInput(config);
+  }
+
+  private validateInput(envConfig: EnvConfig): never | ValidatedEnvConfig {
+    const envVarsSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(Envs)
+        .default(Env.development),
+      PORT: Joi.number().default(3000),
+      API_AUTH_ENABLED: Joi.boolean().required(),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    } else {
+      return (validatedEnvConfig as unknown) as ValidatedEnvConfig;
+    }
   }
 
   get(key: string): string {
